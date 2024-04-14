95a6fcfb161ab28603bae96e748827368aa601ab
Sibongiseni Ndebele <43204005+Sbongslany@users.noreply.github.com>
2024-04-14 18:57:51 +0200

    model and repository

--- lib/repository/repositories.dart
+++ lib/repository/repositories.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 import 'package:http/http.dart';
 
+import '../models/character_model.dart';
+
 class CharacterRepository {
   String url = "https://rickandmortyapi.com/api/character";
-  getCharacters() async {
+  Future<List<CharacterModel>>getCharacters() async {
     Response response = await get(Uri.parse(url));
     if (response.statusCode == 200) {
       final List result = jsonDecode(response.body)['results'];
+      return result.map(((e) => CharacterModel.fromJson(e))).toList();
     } else {
       throw Exception(response.reasonPhrase);
     }
